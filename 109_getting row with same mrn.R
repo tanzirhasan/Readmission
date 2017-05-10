502c0f5994b3ab38c708d6d757286378bf2ace02
tanzirhasan <hitanzir@gmail.com>
2017-05-10 13:33:41 -0700

    Junk removed

--- 109_getting row with same mrn.R
+++
@@ -1,8 +0,0 @@
-# getting the row with MRN number that appeared more than once
-ZSFGH_x <-ZSFGH_c[(duplicated(ZSFGH_c$MRN, fromLast = FALSE)|duplicated(ZSFGH_c$MRN, fromLast = TRUE)),]
-
-head(ZSFGH_x)
-View(ZSFGH_x)
-
-
-
