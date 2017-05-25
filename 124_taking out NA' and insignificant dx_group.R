dd50c6e27bdc9cd48ffd6d499c161960c1484677
tanzirhasan <hitanzir@gmail.com>
2017-05-24 23:04:18 -0700

    slides add

--- 124_taking out NA' and insignificant dx_group.R
+++ 124_taking out NA' and insignificant dx_group.R
@@ -7,9 +7,46 @@ ZSFGH_i <-ZSFGH_h[!(is.na(ZSFGH_h$dx_group)) & !(ZSFGH_h$dx_group=="Factors infl
 ggplot(filter(ZSFGH_i,status=="Readmitted"),aes(dx_group, fill= dx_group))+ geom_bar(aes(y = (..count..)/sum(..count..)))+
   #geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.25)+
   scale_y_continuous(labels = percent)+ theme(axis.title.y=element_blank(),axis.text.x = element_text(angle = 90, hjust = 1),legend.position="none")+
-  ggtitle("Distribution of dx group by readmission status")
+  ggtitle("Distribution of readmitted patients by dx group")
 nrow(ZSFGH_i)
 #8691
+ggplot(filter(ZSFGH_i,status=="Readmitted"),aes(homelessness, fill= homelessness))+ geom_bar(aes(y = (..count..)/sum(..count..)))+
+  #geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.25)+
+  scale_y_continuous(labels = percent)+ theme(axis.title.y=element_blank(),axis.text.x = element_text(angle = 90, hjust = 1),legend.position="none")+
+  ggtitle("Distribution of readmitted patients by homelessness")
+
+
+
+ggplot(ZSFGH_i,aes(homelessness,fill=status))+ geom_bar(aes(y = (..count..)/sum(..count..)),position = "dodge")+
+  geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.25)+
+  scale_y_continuous(labels = percent)+ theme(axis.title.y=element_blank(),axis.text.x = element_text(angle = 90, hjust = 1))+ ggtitle("Distribution of patients by homelessness")
+
+nrow(ZSFGH_i)
+#######
+# total length of stay
+ggplot(ZSFGH_i,aes(tot_len_of_stay,fill= status)) + geom_density() + facet_grid(~status)+ggtitle("Distribution of total length of stay")
+#ggplot(ZSFGH_i,aes(x= status,y=tot_len_of_stay,fill= status)) + geom_boxplot()+ggtitle("Distribution of total length of stay")
+#Hb level
+ggplot(ZSFGH_i,aes(Hb,fill= status)) + geom_histogram(position = 'dodge',binwidth = 4) +ggtitle("Distribution of Hemoglobin by readmission status")
+ggplot(ZSFGH_i,aes(Hb,fill= status)) +geom_density(alpha=0.5)
+
+ggplot(ZSFGH_i,aes(x=status,y=Hb,fill= status)) + geom_boxplot() + labs(x="30-day readmission status",y= "Hemoglobin level")+ ggtitle("Distribution of Hb level by 30-day readmission status")
+
+
+
+#Na level
+ggplot(ZSFGH_i,aes(Na,fill= status)) + geom_histogram(position = 'dodge',binwidth = 10) +ggtitle("Distribution of Sodium by readmission status")
+ggplot(ZSFGH_i,aes(Na,fill= status)) +geom_density(alpha=0.5)
+
+ggplot(ZSFGH_i,aes(x=status,y=Na,fill= status)) + geom_boxplot() + labs(x="30-day readmission status",y= "Sodium level")+ ggtitle("Distribution of Na level by 30-day readmission status")
+
+
+
+
+#####
+
+
+
 
 # getting the row with MRN number that appeared more than once
 ZSFGH_x <-ZSFGH_i[(duplicated(ZSFGH_i$MRN, fromLast = FALSE)|duplicated(ZSFGH_i$MRN, fromLast = TRUE)),]
@@ -39,28 +76,33 @@ nrow(ZSFGH_x %>% group_by(MRN)%>%filter(MRN %in% catch)%>% slice(which.min(Inter
 # 593
 data_mod_raw <- ZSFGH_x %>% group_by(MRN)%>%filter(MRN %in% catch)%>% slice(which.min(Interval))
 data_mod_raw1 <- as.data.frame(data_mod_raw)
-data_mod_raw1$ulti_stat <-rep(1,nrow(data_mod_raw))
+data_mod_raw1$ulti_stat <-rep("yes",nrow(data_mod_raw))
 class(data_mod_raw1)
 data_rest <-ZSFGH_x[!(ZSFGH_x$MRN %in%catch),] 
 View(data_rest)
 data_rest1 <- subset(data_rest, data_rest$Interval== 0)
 class(data_mod_raw1)
 class(data_rest1)
-data_rest1$ulti_stat <- rep(0,nrow(data_rest1))
+data_rest1$ulti_stat <- rep("no",nrow(data_rest1))
 
 data_inter <- rbind(data_mod_raw1, data_rest1)
 View(data_inter)
 data_inter$ulti_stat <- as.factor(data_inter$ulti_stat)
 
-
+str(data_inter)
 View(data_inter)
 
 nrow(data_inter)
 colnames(data_inter)
 
 
-data_mod <- data_inter[c(-1,-2, -5,-14, -16)]
+data_mod <- data_inter[c(-1,-2, -5,-9,-10,-14, -16)]
 head(data_mod)
 
 write.csv(data_mod, "data_mod.csv", row.names = FALSE)
 
+
+t.test(Hb~ status, data=ZSFGH_i)
+#t.test(Hb~ status, data=ZSFGH_i, subset = status %in% c("Readmitted","Not_readmitted"))
+t.test(Na~ status, data=ZSFGH_i)
+t.test(tot_len_of_stay~ status, data=ZSFGH_i)
--- 208_ model fitting.R
+++ 208_ model fitting.R
@@ -2,18 +2,44 @@
 
 data_mod1 <-read.csv("data_mod.csv", header = TRUE)
 data_mod1$ulti_stat <-as.factor(data_mod1$ulti_stat)
+colnames(data_mod1)
 head(data_mod1)
 str(data_mod1)
-fit1 <-glm(ulti_stat~ race_cd+ gender_cd+ tot_len_of_stay+hosp_svc+dx_group+Hb+Na+age+ homelessness,family = binomial(),data = data_mod1)
+
+
+ggplot(filter(data_mod1,ulti_stat==1),aes(dx_group, fill= dx_group))+ geom_bar(aes(y = (..count..)/sum(..count..)))+
+  #geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.25)+
+  scale_y_continuous(labels = percent)+ theme(axis.title.y=element_blank(),axis.text.x = element_text(angle = 90, hjust = 1),legend.position="none")+
+  ggtitle("Distribution of dx group by readmission status")
+require(GGally)
+ggpairs(data_mod1[sapply(data_mod1,function(x)!is.factor(x))])
+data_remod <- data_mod1
+data_remod$cancer <- factor(rep("yes",nrow(data_remod)),levels = c("yes", "no")) 
+data_remod$cancer <- ifelse(data_remod$dx_group=="Neoplasms","yes","no")
+data_remod$digesdis <- factor(rep("yes",nrow(data_remod)),levels = c("yes", "no")) 
+data_remod$digesdis <- ifelse(data_remod$dx_group=="digestive system","yes","no")
+head(data_remod)
+data_remod$higlander <- factor(rep("yes",nrow(data_remod)),levels = c("yes", "no")) 
+data_remod$higlander <- ifelse(data_remod$race_cd=="Pacific Islander","yes","no")
+data_remod$homelessness1 <- factor(rep("yes",nrow(data_remod)),levels = c("yes", "no"))
+data_remod$homelessness1 <-  ifelse(data_remod$homelessness=="homeless","yes","no")
+data_remod$asian <- factor(rep("yes",nrow(data_remod)),levels = c("yes", "no")) 
+data_remod$asian <- ifelse(data_remod$race_cd=="Asian","yes","no")
+fit1 <-glm(ulti_stat~   tot_len_of_stay+Hb+Na+age+ homelessness1+higlander ,family = binomial(),data = data_remod)
 summary(fit1)
 plot(fit1)
+
+#
+fit1 <-glm(ulti_stat~ race_cd+ gender_cd+ tot_len_of_stay+hosp_svc+dx_group+Hb+Na+age+ homelessness,family = binomial(),data = data_mod1)
+summary(fit1)
 
 
 fit2 <-glm(ulti_stat~ race_cd+ gender_cd+ tot_len_of_stay+hosp_svc+dx_group+Hb+Na+ homelessness,family = binomial(),data = data_mod1)
 summary(fit2)
 
 
-# Step 
+
+##
 fit_null <- glm(ulti_stat~ 1, family = binomial(),data = data_mod1)
 summary(fit_null)
 
@@ -22,11 +48,24 @@ summary(fit_main)
 
 fit_step1 <- step(fit_null, scope = list(lower=fit_null,upper=fit_main),direction = "both")
 
+fit_mod <- glm(ulti_stat~race_cd + Hb,family = binomial(),data = data_mod1)
+summary(fit_mod)
+
+
+# Step 
+fit_null1 <- glm(ulti_stat~ 1, family = binomial(),data = data_remod)
+summary(fit_null1)
+
+fit_main1 <- glm(ulti_stat~.,family = binomial(),data = data_remod)
+summary(fit_main1)
+
+fit_step11 <- step(fit_null, scope = list(lower=fit_null,upper=fit_main),direction = "both")
+
 fit_some_inter <-glm(ulti_stat ~ AdmitCount + tot_len_of_stay + age+Hb+Na+homelessness+dx_group,family = binomial(),data = data_mod1)
 summary(fit_some_inter)
 
-fit_step2 <- step(fit_null, scope = list(lower=fit_null,upper=fit_some_inter),direction = "both")
-summary(fit_step2 )
+fit_step21 <- step(fit_null, scope = list(lower=fit_null,upper=fit_some_inter),direction = "both")
+summary(fit_step21 )
 
 
 
@@ -49,6 +88,7 @@ logisticPseudoR2s <- function(LogModel){
 }
 
 logisticPseudoR2s(fit_step1)
+logisticPseudoR2s(fit_mod)
 # 
 # Pseudo R^2 for  logistic regression
 # Hosmer and Lameshow R^2  1 
